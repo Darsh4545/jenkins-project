27af2c09976eec3a4e71af06a47a8c6b7fb3c880
Cloud-Devops <124419201+Darsh4545@users.noreply.github.com>
2024-11-16 19:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from werkzeug.utils import quote
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return "Hello, This is a simple Python app running in a container."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
